1bc0d6b7c09efa0b430b9fd2b159fd002c4b54aa
Rui Chen <rui@chenrui.dev>
2024-08-16 07:40:42 -0400

    avro-cpp 1.12.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/avro-cpp.rb
+++ Formula/a/avro-cpp.rb
@@ -5,10 +5,9 @@ class AvroCpp < Formula
   # https://github.com/Homebrew/homebrew-core/pull/146296#issuecomment-1737945877
   # https://apple.stackexchange.com/questions/197839/why-is-extracting-this-tgz-throwing-an-error-on-my-mac-but-not-on-linux
   url "https://github.com/apache/avro.git",
-      tag:      "release-1.11.3",
-      revision: "35ff8b997738e4d983871902d47bfb67b3250734"
+      tag:      "release-1.12.0",
+      revision: "8c27801dc8d42ccc00997f25c0b8f45f8d4a233e"
   license "Apache-2.0"
-  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "9c18b3e6a70662c65940c9c801ee5922ce4e544bc72d43e46d152c82f2ecf9e8"
